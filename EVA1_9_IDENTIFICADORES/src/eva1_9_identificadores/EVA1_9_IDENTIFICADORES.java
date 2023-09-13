58b1e8d21f60c5b26643d5885875476eab9bbf4c
ihdzg <143562753+ihdzg@users.noreply.github.com>
2023-09-13 17:53:46 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_identificadores;
+
+/**
+ *
+ * @author ihdzg
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int 1nombre; noimbre no valido, empieza con un numero
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int nombre;
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable nombre ya existe previamente
+        
+        //int public; no podemos usar public, es palabre reservada (keyword)
+        int Public;
+        double knlsdcmnkepii;
+    }
+    
+}
