6f4c84c96ec8ea1f9fe9f313a5eb59808a75b993
ihdzg <143562753+ihdzg@users.noreply.github.com>
2023-09-18 17:51:17 -0600

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author ihdzg
+ */
+public class EVA1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 0; //INICIALIZANDO LA VARIABLE
+        System.out.println(valor);
+
+        int precio; //declarar
+        precio = 100; //inicializar
+
+        double salario = 0, precioMayoreo = 100, iva = 10.5, subtotal = 15, total = 100.0; //tenemos 5 variables, TODAS, de tipo int
+
+        //CONSTANTES 
+        //final---> TE PERIMITE ASIGNAR UN VALOR SOLO UNA VEZ
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+
+        final double MI_VALOR_PI = 3.1416;
+        final String ESCUELA = "Instituto Tecnologico de Chihuahua II";
+        final int CALIFA_PASAR = 70;
+        /*
+            Varias lianeas de comentarios
+            Cuando quieren tirar mucho rollo
+            Esta es la mejor opcion
+        */
+        
+        int x; //COMENTARIO DE UNA SOLA LINEA, TODO EN LA LINEA ES COMENTARIO
+        
+
+    }
+
+}
