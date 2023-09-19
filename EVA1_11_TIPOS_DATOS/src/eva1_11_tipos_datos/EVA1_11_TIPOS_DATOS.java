0c772f312cb955519c76ab44963a015bb072c556
ihdzg <143562753+ihdzg@users.noreply.github.com>
2023-09-19 17:49:06 -0600

    Add files via upload

---
+++ EVA1_11_TIPOS_DATOS/src/eva1_11_tipos_datos/EVA1_11_TIPOS_DATOS.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_11_tipos_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ihdzg
+ */
+public class EVA1_11_TIPOS_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte califa;
+        califa = 127;
+        //califa = 200;
+        califa = -128;
+        //califa = -200
+        //califa = 127 + 1;
+        int valor;
+        valor = Integer.MAX_VALUE;
+        System.out.println(valor + 1);
+        valor = Integer.MIN_VALUE;
+        System.out.println(valor);
+        
+        
+        //---------------------------------
+        
+        char caracter = 'A';
+        System.out.println(caracter);
+        System.out.println(caracter + 1);
+        
+    }
+    
+}
